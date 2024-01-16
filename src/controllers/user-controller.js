a5a61bf2867551e9013a1cd6533083539e3b6ec9
Harshit Shukla <mrharshit2409@gmail.com>
2024-01-16 19:28:38 +0530

    Added user schema and adding users to the db cloud

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,9 +1,13 @@
+const User = require('../models/user-model');
+const mongoose = require('mongoose');
+
 exports.homepage = async(req, res) => {
+    const messages = await req.flash('info');
     const locals = {
         title: 'NodeJS',
         description: 'NodeJS User Management System'
     }
-    res.render('index', locals);
+    res.render('index', {locals, messages});
 }
 
 exports.addUser = async(req, res) => {
@@ -15,9 +19,19 @@ exports.addUser = async(req, res) => {
 }
 
 exports.postUser = async(req, res) => {
-    const locals = {
-        title: 'New User added',
-        description: 'NodeJS User Management System'
-    }
-    res.render('user/add', locals);
+    const newUser = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        tel: req.body.tel,
+        email: req.body.email,
+        details: req.body.details,
+    });
+    try {
+        await User.create(newUser);
+        await req.flash("info", "New user has been added.");
+
+        res.redirect("/");
+    } catch (error) {
+        console.log(error);
+    } 
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 const connectDB = require('./config/db');
+const flash = require('connect-flash');
+const session = require('express-session');
 const { PORT } = require('./config/serverConfig');
 
 const app = express();
@@ -11,6 +13,19 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use(express.static('public'));
+
+app.use(
+    session({
+        secret: 'secret',
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+        }
+    })
+);
+
+app.use(flash({ sessionKeyName: 'flashMessage' }));
 
 app.use(expressLayout);
 app.set('layout', './layouts/main');
